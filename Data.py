d869ddad862bcd3ef8c6b905a3bbff8c12c7bdb2
Ruben103 <R.D.Schut94@gmail.com>
2020-10-21 10:51:16 +0200

    kenker

--- Data.py
+++ Data.py
@@ -5,8 +5,8 @@ from sklearn.preprocessing import OneHotEncoder
 from datetime import datetime
 from numpy import datetime64
 
-class Data:
 
+class Data:
 
     def unzip_COP_data(self, zipfilename):
         # Filebane should be : "COP.filt3.sub.zip"
@@ -19,14 +19,15 @@ class Data:
             print("File", zipfilename, "was not found in directory.")
 
     def read_data(self, directory):
-        print("\nReading data..."); start_time = datetime.utcnow()
+        print("\nReading data...")
+        start_time = datetime.utcnow()
         list_of_items = os.listdir(directory)
         for elem in list_of_items:
-            if os.path.splitext(elem)[1]!= '.json':
+            if os.path.splitext(elem)[1] != '.json':
                 path = os.path.join(os.getcwd(), directory, elem)
                 os.remove(path)
 
-        #make sure all files are .json files
+        # make sure all files are .json files
         list_of_items = os.listdir(directory)
         data = None
 
@@ -51,19 +52,20 @@ class Data:
         return data
 
     def read_bodies(self, data, save_bodies=True):
-        #takes around 2 minutes 20 seconds on my Macbook Pro
-        print("\nReading bodies..."); start_time = datetime.utcnow()
+        # takes around 2 minutes 20 seconds on my Macbook Pro
+        print("\nReading bodies...")
+        start_time = datetime.utcnow()
         bodies = DataFrame(columns=['date', 'body', 'year'])
 
         for id in range(data.shape[0]):
             article = data.iloc[id]['articles']
             dtime = to_datetime(article['date'])
             body = article['body']
-            bodies = bodies.append(DataFrame(data=[[dtime, body, dtime.year]], columns=['date', 'body', 'year']), ignore_index=True)
+            bodies = bodies.append(DataFrame(data=[[dtime, body, dtime.year]], columns=['date', 'body', 'year']))
         print("Done.", 'Time:', datetime.utcnow() - start_time)
         if save_bodies:
             print("Saving bodies to csv")
-            bodies.to_json('bodies.csv')
+            bodies.to_csv('bodies.csv')
         return bodies
 
     def read_saved_bodies(self):
@@ -76,5 +78,3 @@ class Data:
     def vectorise_labels(self, labels):
 
         labels = OneHotEncoder
-
-
--- Experiments.py
+++ Experiments.py
@@ -6,13 +6,16 @@ class Experiments:
         self.data_directory = "COP_filt3_sub"
         self.data_zipfilename = "COP.filt3.sub.zip"
 
+    def settingUp(self):
+        Data().unzip_COP_data(self.data_zipfilename)
+        data = Data().read_data(self.data_directory)
+        bodies = Data().read_bodies(data)
 
     def experimentOne(self):
         # Data().unzip_COP_data(self.data_zipfilename)
 
         data = Data().read_data(self.data_directory)
         bodies = Data().read_bodies(data)
-        bodies2 = Data().read_saved_bodies()
+        # bodies = Data().read_saved_bodies()
 
-        if bodies == bodies2:
-            print("Great success")
+        1==1
